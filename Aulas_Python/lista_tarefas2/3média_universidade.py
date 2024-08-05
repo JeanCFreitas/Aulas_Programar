0bba6ceece5d3198c361cb8e22d2b930dcd653d9
JeanCFreitas <jean.freitas@aluno.ifsp.edu.br>
2024-08-04 22:49:55 -0300

    univ

--- Aulas_Python/lista_tarefas2/3média_universidade.py
+++ Aulas_Python/lista_tarefas2/3média_universidade.py
@@ -3,4 +3,4 @@ n1=float(input('Digite primeira nota:'))
 n2=float(input('Digite segunda nota:'))
 media=(n1+n2)/2
 arredondar=math.ceil(media)
-print(arredondar)
+print(f'Média arrendondada: {arredondar}')
