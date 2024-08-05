9303c2311a396272dadf77918d92f3520da4d5f4
JeanCFreitas <jean.freitas@aluno.ifsp.edu.br>
2024-08-05 20:23:51 -0300

    salafrario_liquido_terminado

--- Aulas_Python/lista_tarefas2/5salario_liquido.py
+++ Aulas_Python/lista_tarefas2/5salario_liquido.py
@@ -12,40 +12,63 @@
 #cada funcionário, e deverá receber como resultado o salário líquido. O usuário poderá 
 #calcular salário para N funcionários, para finalizar o programa o usuário deverá digitar 
 #0 no salário bruto, ao finalizar o programa exibir o total geral dos salários líquidos.
-
+salario_total=0
+contas=0
 salarioB=float(input('Digite o Salário Bruto:'))
-horas=float(input('Digita as horas trabalhadas no mês:'))
-
-
-if (salarioB<800):
-    print(f'Salário Bruto: R$ {salarioB}')
-    print(f'Salário Liquido: R$ {salarioB}')
+while (salarioB!=0):
+    horasexced=0
+    custo_hora=0
+    saliq_total=0
     
-if (800<=salarioB<=1600):
-    imposto=0.08*salarioB
-    encargos=0.05*salarioB
-    saliq=salarioB-imposto-encargos
-    print(f'Salário Bruto: R$ {salarioB}')
-    print(f'Imposto 8%: R$ {imposto}')
-    print(f'Encargos 5%: R$ {encargos}')
-    print(f'Salário Liquido: R$ {saliq}')
+    Horahomem=0
+    imposto=0
+    encargos=0
+    custo_hora=salarioB/160
+ 
+    horas=float(input('Digita as horas trabalhadas no mês:'))
     
-if (salarioB>1600):
-    imposto=0.15*salarioB
-    encargos=0.07*salarioB
-    saliq=salarioB-imposto-encargos
     print(f'Salário Bruto: R$ {salarioB}')
-    print(f'Imposto 8%: R$ {imposto}')
-    print(f'Encargos 5%: R$ {encargos}')
-    print(f'Salário Liquido: R$ {saliq}')
-
-print(f'Horas trabalhadas: {horas}')
-
-if (horas<=160):
-    print(f'Horas excedentes: 0')
-    print('Horas c/50% aplicado: 0')
-else:
-    print(f'Horas excedentes:{horas-160}')
-    print(f'Horas c/50% aplicado: {(horas-160)*1.5}')
+    print(f'Horas trabalhadas: {horas}')
     
+    if (horas<=160):
+        horasexced=0
+        print(f'Horas excedentes: {horasexced} horas')
+        print(f'Horas c/50% aplicado: {horasexced}')
+    
+    else:
+        horasexced=horas-160
+        print(f'Horas excedentes:{horasexced} horas')
+        print(f'Horas c/50% aplicado: {(horasexced)*1.5} horas')
+        Horahomem=salarioB/160
 
+    
+    print(f'Valor hora homem: R$ {Horahomem}')
+    adicional=Horahomem*horasexced*1.5
+    print(f'Valor adicional p/ horas extras: R$ {adicional}')
+
+    if (salarioB<800):
+        
+        print(f'Salário Bruto: R$ {salarioB}')
+
+    if (800<=salarioB<=1600):
+        imposto=0.08*salarioB
+        encargos=0.05*salarioB
+        
+        print(f'Imposto 8%: R$ {imposto}')
+        print(f'Encargos 5%: R$ {encargos}')
+    
+    if (salarioB>1600):
+        imposto=0.15*salarioB
+        encargos=0.07*salarioB
+        
+        print(f'Imposto 15%: R$ {imposto}')
+        print("Encargos 7%: R$" ,'%.2f' %encargos)
+    
+    saliq=salarioB+adicional-imposto-encargos
+    print(f'Salário liquido: R$ {saliq}')
+    salario_total=salario_total+saliq
+    contas=contas+1
+    salarioB=float(input('Digite o Salário Bruto:'))
+
+print(f'Total de Salários Liquidos calculados: R$ {salario_total}')
+print(f'Você inseriu informações de {contas} funcionarios')
