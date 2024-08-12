fd97eb6e27d2fbe390f4db9ac26ca38541436496
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-08-12 09:01:48 -0300

    soma

--- Aulas_Python/lista_tarefas3/1inteiro_impar.py
+++ Aulas_Python/lista_tarefas3/1inteiro_impar.py
@@ -1,6 +1,5 @@
-
-while (num<20)
-if num%2==0:
-    print(f"{num} é Par")
-else:
-    print(f"{num} é Impar")
+num=0
+while (num<20):
+    if num%2!=0:
+        print(f"{num} é Impar")
+    num=num+1
