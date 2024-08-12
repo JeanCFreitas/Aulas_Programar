9911b052853500c824255551e5d5da1d3dc886bf
JeanCFreitas <163172637+JeanCFreitas@users.noreply.github.com>
2024-08-12 11:44:19 -0300

    Delete Aulas_Python/lista_tarefas3 directory

--- Aulas_Python/lista_tarefas3/1inteiro_impar.py
+++
@@ -1,5 +0,0 @@
-num=0
-while (num<20):
-    if num%2!=0:
-        print(f"{num} é Impar")
-    num=num+1
--- Aulas_Python/lista_tarefas3/2soma.py
+++
@@ -1,6 +0,0 @@
-num=0
-soma=0
-while (num<100):
-    num=num+1
-    soma=soma+num
-print (soma)
