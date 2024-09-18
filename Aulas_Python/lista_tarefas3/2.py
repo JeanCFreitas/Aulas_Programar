0ef200cd1527ba0f9ff4a4ee9fadac2fb1a51df3
JeanCFreitas <163172637+JeanCFreitas@users.noreply.github.com>
2024-09-18 14:59:47 -0300

    Delete Aulas_Python/lista_tarefas3 directory

--- Aulas_Python/lista_tarefas3/2.py
+++
@@ -1,5 +0,0 @@
-num=20
-while(num>0):
-    if num%2==0:
-        print(f"O número {num} é Par")
-    num=num-1
--- Aulas_Python/lista_tarefas3/3.py
+++
@@ -1,7 +0,0 @@
-num=0
-while(num<=20):
-    if num%2==0:
-        print(f"{num} é Par")
-    else:
-        print(f"{num} é Impar")
-    num=num+1
