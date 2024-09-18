83e6992425fd8051608c007e7c27f25fca3fc841
JeanCFreitas <163172637+JeanCFreitas@users.noreply.github.com>
2024-09-18 15:00:06 -0300

    Delete Aulas_Python/lista_tarefas1 directory

--- Aulas_Python/lista_tarefas1/2fahrenheit_celsius.py
+++
@@ -1,3 +0,0 @@
-F = float(input("Digite a temperatura em graus Fahrenheit:"))
-C = ((F-32)*5)/9
-print("Temperatura em Celsius:", '%.2f' %C,"ºC")
--- Aulas_Python/lista_tarefas1/3volume.py
+++
@@ -1,4 +0,0 @@
-raio = float(input("Digite o raio:"))
-altura = float(input("Digite a altura:"))
-volume = 3.14159*((raio**2)*altura)
-print("Volume:", '%.2f' %volume)
