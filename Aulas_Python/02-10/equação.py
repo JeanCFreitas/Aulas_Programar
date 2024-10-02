8321be2b1b1d0c705c24f22559286a39c7501423
Aluno cansado <estudante1@debian.spo.ifsp.edu.br>
2024-10-02 16:22:43 -0300

    agora_Foi

--- Aulas_Python/02-10/equação.py
+++ Aulas_Python/02-10/equação.py
@@ -1,11 +1,16 @@
 import math
-def equacao(a,b,c):
-    if a == 0:
-        print('Não é equação de segundo grau')
-    delta=b**2-4*a*c
-    if delta<0:
-        print('não é equação')
-    else:
-        raiz1= (-b + math.sqrt(delta))/(2*a)
-        raiz2= (-b - math.sqrt(delta))/(2*a)
-        print  (f'A equação possui duas raizes reais: {raiz1} e {raiz2}')
+a = float(input("Digite A:"))
+b = float(input("Digite B:"))
+c = float(input("Digite C:"))
+    
+if a == 0:
+    print('Não é equação de segundo grau')
+delta=b**2-4*a*c
+if delta<0:
+    print('não é equação')
+elif delta == 0:
+    print('nao')
+else:
+    raiz1= (-b + math.sqrt(delta))/(2*a)
+    raiz2= (-b - math.sqrt(delta))/(2*a)
+    print(f'A equação possui duas raizes reais: {raiz1} e {raiz2}')
