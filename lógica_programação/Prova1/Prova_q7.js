e3962c8510ffc5ec4882c4f9a092cd17659c5931
JeanCFreitas <163172637+JeanCFreitas@users.noreply.github.com>
2025-04-29 13:59:34 -0300

    Arrumado

--- lógica_programação/Prova1/Prova_q7.js
+++ lógica_programação/Prova1/Prova_q7.js
@@ -2,13 +2,13 @@ let x = 15
 let y = 2
 
 console.log('X = ' + x)
-console.log('y = ' + y)
-console.log('-x = ' + (-x))
-console.log('x/y = ' + (x/y))
+console.log('Y = ' + y)
+console.log('-X = ' + (-x))
+console.log('X/Y = ' + (x/y))
 console.log('Resto de X por Y: ' + (x%y))
 console.log('Inteiro de X por Y: '+ (Math.floor(x/y)))
-console.log('x + 1 = ' + (++x))
+console.log('X + 1 = ' + (++x))
 console.log('X = ' + x)
-console.log('x + 1 = ' + (x++))
+console.log('X + 1 = ' + (x++))
 console.log('X = ' + x)
 console.log(Math.floor(x))
