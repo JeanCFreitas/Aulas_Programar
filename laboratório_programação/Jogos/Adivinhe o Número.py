4d529a0826aeace612e4438fc05c38001b4a93e4
JeanCFreitas <jean.freitas@aluno.ifsp.edu.br>
2025-06-09 01:40:03 -0300

    ajuda na adivinhação

--- laboratório_programação/Jogos/Adivinhe o Número.py
+++ laboratório_programação/Jogos/Adivinhe o Número.py
@@ -10,20 +10,25 @@ def adivinhação():
     tentativas = 0
     chute = 0
     print("Adivinhe o número entre 1 e 100.\n")
+    perto = 0
 
     while chute!=numero:
         
         chute = int(input("Digite seu palpite: "))
         tentativas += 1
+        perto = numero - chute
 
         if chute < 1 or chute > 100:
             print("Por favor, digite um número entre 1 e 100.")
         elif chute < numero:
             print("Muito baixo. Tente novamente.")
+            if perto <=5:
+                print(f"Mais {perto} digitos para cima")
         elif chute > numero:
             print("Muito alto. Tente novamente.")
-        if chute == numero+5 or chute == numero-5:
-            print("Está chegando perto")
+            if perto <=5:
+                print(f"Mais {perto} digitos para baixo")
+
 
     print(f"Parabéns! Você descobriu o número {numero} em {tentativas} tentativa(s)!")
             
