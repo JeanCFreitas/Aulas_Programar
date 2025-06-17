dd17e5bf75514abb4b2c96bb1b3ba6016f9ffb5f
JeanCFreitas <jean.freitas@aluno.ifsp.edu.br>
2025-06-16 21:12:36 -0300

    calc

--- lógica_programação/10-06/calc.js
+++ lógica_programação/10-06/calc.js
@@ -1,24 +1,25 @@
+    function calculardora() {
+      const num1 = parseFloat(document.getElementById('num1').value);
+      const num2 = parseFloat(document.getElementById('num2').value);
+      const operador = document.getElementById('operador').value;
+      let resultado;
 
-const soma = function(a, b) {return a+b}
-const sub = function(a, b) {return a-b}
-const mult = function(a, b) {return a*b}
-const div = function(a, b) {return a/b}
 
-
-while (true){
-    let operacao, a=15, b=3
-    console.log("Calculadora\n")
-    console.log("adicao/subtracao/multiplicacao/divisao")
-    operacao = 1
-    switch (operacao){
-        case 1:
-            console.log("a soma entre " + a + " e " + b + " é " + soma(a, b))
-        case 2:
-            console.log("a soma entre " + a + " e " + b + " é " + sub(a, b))
-        case 3:
-            console.log("a soma entre " + a + " e " + b + " é " + mult(a, b))
-        case 4:
-            console.log("a soma entre " + a + " e " + b + " é " + div(a, b))
+      switch (operador) {
+        case '+':
+          resultado = num1 + num2;
+          break;
+        case '-':
+          resultado = num1 - num2;
+          break;
+        case '*':
+          resultado = num1 * num2;
+          break;
+        case '/':
+          resultado = num2 !== 0 ? (num1 / num2) : "Erro: divisão por zero.";
+          break;
+        default:
+          resultado = "Operador inválido.";
+        }
     }
-}
-
+      document.getElementById('resultado').innerText = `Resultado: ${resultado}`;
