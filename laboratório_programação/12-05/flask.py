93aad81bd9ace8f6cac3c5552fd414c486622db4
JeanCFreitas <163172637+JeanCFreitas@users.noreply.github.com>
2026-03-04 01:06:04 -0300

    Delete laboratório_programação directory

--- laboratório_programação/12-05/flask.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def ola_mundo():
-    return 'olá mundo flask'
-if __name__ == '__main__':
-    app.run(debug=True)
--- laboratório_programação/19-05/teste.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-app = Flask('/bemvindo/pag')
-@app.route('/')
-def ola_mundo(nome):
-    return render_template('pag.html', usuario=nome)
