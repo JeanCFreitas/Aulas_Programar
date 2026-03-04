286eed643dd1250e793e8e0eab111f8882b522ab
JeanCFreitas <163172637+JeanCFreitas@users.noreply.github.com>
2026-03-04 01:05:26 -0300

    Delete lógica_programação/03-06 directory

--- lógica_programação/03-06/pratoDia.js
+++
@@ -1,3 +0,0 @@
-/*historinha: um restaurante oferece um cardápio, onde cada dia da semana tem um prato especial.
-pratos: lasanha, feijoada, frango assado, peixe grelhado, picanha, pizza, churrasco.
-utilizando o switch case, exiba o prato do dia*/
